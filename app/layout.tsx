092842cac63e85da3bc48be731edfa0a256024e6
Victor <victor@Victors-MacBook-Air.local>
2026-03-26 19:20:07 +0400

    added yandex-metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { getSiteSettings } from "@/lib/actions";
 import PagePreloader from "@/components/PagePreloader";
+import Script from "next/script";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -89,6 +90,25 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               ]),
             }}
           />
+          {/* Yandex Metrika */}
+          <Script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108254799','ym');
+                ym(108254799,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",accurateTrackBounce:true,trackLinks:true});
+              `,
+            }}
+          />
+          <noscript>
+            <img src="https://mc.yandex.ru/watch/108254799" style={{ position: "absolute", left: -9999 }} alt="" />
+          </noscript>
           <PagePreloader />
           {children}
         </body>
