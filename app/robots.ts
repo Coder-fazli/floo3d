4d172c3a466a0bdcca40ed5f8fb5236aaea10d0f
Victor <victor@Victors-MacBook-Air.local>
2026-03-28 01:20:13 +0400

    error fixed

--- app/robots.ts
+++ app/robots.ts
@@ -4,8 +4,7 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
-            allow: '/',
-            allow: ['/2d-to-3d-floor-plan-converter'],
+            allow: ['/', '/2d-to-3d-floor-plan-converter'],
             disallow: ['/secure-7x9/', '/api/', '/dashboard/', '/sign-in/', '/sign-up/'],
         },
         sitemap: 'https://myhomestyler.com/sitemap.xml',
