6d74c6f42addb0e5d1828bd84c243343772cd111
Victor <victor@Victors-MacBook-Air.local>
2026-04-03 12:01:53 +0400

    NEw seo Things

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,25 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: ['/', '/2d-to-3d-floor-plan-converter'],
-            disallow: ['/secure-7x9/', '/api/', '/dashboard/', '/sign-in/', '/sign-up/'],
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: ['/', '/2d-to-3d-floor-plan-converter'],
+                disallow: ['/secure-7x9/', '/api/', '/dashboard/', '/sign-in/', '/sign-up/'],
+            },
+            {
+                userAgent: 'GPTBot',
+                allow: ['/', '/2d-to-3d-floor-plan-converter'],
+            },
+            {
+                userAgent: 'PerplexityBot',
+                allow: ['/', '/2d-to-3d-floor-plan-converter'],
+            },
+            {
+                userAgent: 'ClaudeBot',
+                allow: ['/', '/2d-to-3d-floor-plan-converter'],
+            },
+        ],
         sitemap: 'https://myhomestyler.com/sitemap.xml',
     }
 }
