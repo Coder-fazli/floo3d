65e2df940f3477fd0588ca8405a1aed11df8b11b
Victor <victor@Victors-MacBook-Air.local>
2026-04-04 01:26:42 +0400

    google prolem

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ const isPublicRoute = createRouteMatcher([
   "/2d-to-3d-floor-plan-converter(.*)",
   "/api/guest-generate(.*)",
   "/api/webhooks(.*)", 
-  "/floor-plan-generator#generator(.*)"
+  "/floor-plan-generator(.*)"
 ]);
 
 const isAdminRoute = createRouteMatcher(["/secure-7x9(.*)"]);
